f461edf2953c134cdb88915989821228a74420d6
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-08-05 11:11:44 +0100

    complete workshop

--- exercise/router.js
+++ exercise/router.js
@@ -1,34 +1,53 @@
-'use strict';
+"use strict";
 
-const { parse } = require('url');
-const { readFile } = require('fs');
-const path = require('path')
+const { parse } = require("url");
+const { readFile } = require("fs");
+const path = require("path");
 const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
 
 module.exports = (req, res) => {
   switch (`${req.method} ${req.url}`) {
-    case 'GET /':
-      return readFile(
-        path.join(__dirname, 'index.html'),
-        (err, data) => {
-          res.writeHead(
-            200,
-            {
-              'Content-Type': 'text/html',
-              'Content-Length': data.length
-            }
-          );
-          return res.end(data);
-        }
-      );
+    case "GET /":
+      return readFile(path.join(__dirname, "index.html"), (err, data) => {
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+          "Content-Length": data.length,
+        });
+        return res.end(data);
+      });
+    case "POST /login":
+      res.writeHead(302, {
+        "Set-Cookie": "logged_in=true; HttpOnly; Max-Age=9000;",
+        Location: "/",
+      });
+      return res.end();
+    case "POST /logout":
+      res.writeHead(302, {
+        "Set-Cookie": "logged_in=false; HttpOnly; Max-Age=0;",
+        Location: "/",
+      });
+      return res.end();
+    case "GET /auth_check":
+      if (req.headers.cookie && req.headers.cookie.match(/logged_in=true/)) {
+        const message = "success!";
+        res.writeHead(200, {
+          "Content-Type": "text/plain",
+          "Content-Length": message.length,
+        });
+        return res.end(message);
+      } else {
+        const message = "fail!";
+        res.writeHead(401, {
+          "Content-Type": "text/plain",
+          "Content-Length": message.length,
+        });
+        return res.end(message);
+      }
     default:
-      res.writeHead(
-        404,
-        {
-          'Content-Type': 'text/html',
-          'Content-Length': notFoundPage.length
-        }
-      );
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+        "Content-Length": notFoundPage.length,
+      });
       return res.end(notFoundPage);
   }
-}
+};
